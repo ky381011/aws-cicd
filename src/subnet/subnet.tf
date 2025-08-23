06a9529e4369188cbbbb3dc307332da1cdaf3970
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-08-23 16:53:41 +0900

    Add more subnet

--- src/subnet/subnet.tf
+++ src/subnet/subnet.tf
@@ -5,3 +5,27 @@ resource "aws_subnet" "subnet0" {
     Name = "tf_test"
   }
 }
+
+resource "aws_subnet" "subnet4" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "172.16.4.0/24"
+  tags = {
+    Name = "tf_test"
+  }
+}
+
+resource "aws_subnet" "subnet8" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "172.16.8.0/24"
+  tags = {
+    Name = "tf_test"
+  }
+}
+
+resource "aws_subnet" "subnet12" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "172.16.12.0/24"
+  tags = {
+    Name = "tf_test"
+  }
+}
