3055e10582e442d8c8da02947906c08cf02a0303
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-08-25 21:00:23 +0900

    Change tp use for_each

--- src/subnet/subnet.tf
+++ src/subnet/subnet.tf
@@ -1,30 +1,18 @@
-resource "aws_subnet" "subnet0" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "172.16.0.0/24"
-  tags = {
-    Name = "tf_test"
+variable "subnet_cidrs" {
+  default = {
+    subnet0  = "172.16.0.0/24"
+    subnet4  = "172.16.4.0/24"
+    subnet8  = "172.16.8.0/24"
+    subnet12 = "172.16.12.0/24"
   }
 }
 
-resource "aws_subnet" "subnet4" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "172.16.4.0/24"
-  tags = {
-    Name = "tf_test"
-  }
-}
+resource "aws_subnet" "subnets" {
+  for_each = var.subnet_cidrs
 
-resource "aws_subnet" "subnet8" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "172.16.8.0/24"
-  tags = {
-    Name = "tf_test"
-  }
-}
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = each.value
 
-resource "aws_subnet" "subnet12" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "172.16.12.0/24"
   tags = {
     Name = "tf_test"
   }
