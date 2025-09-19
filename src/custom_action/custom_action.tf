2bb4ba32b3b51d4051533d2308b1e0a9ed7faab3
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-09-19 23:19:00 +0900

    Delete resource for decremental apply

--- src/custom_action/custom_action.tf
+++ src/custom_action/custom_action.tf
@@ -6,14 +6,3 @@ resource "aws_vpc" "test_state"{
     deploy = "github_actions"
   }
 }
-
-resource "aws_subnet" "public_subnet_1a" {
-  vpc_id                  = aws_vpc.test_state.id
-  cidr_block              = "172.16.0.0/24"
-  availability_zone       = "ap-northeast-1a"
-
-  tags = {
-    Name   = "tf_test-subnet"
-    deploy = "github_actions"
-  }
-}
