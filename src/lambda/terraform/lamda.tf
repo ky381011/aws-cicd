3fda2802fff5201ba35598c0bbcc99c9ce6bef0a
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-10-03 23:02:04 +0900

    Add lambda resource

--- src/lambda/terraform/lamda.tf
+++ src/lambda/terraform/lamda.tf
@@ -8,3 +8,20 @@ terraform {
 }
 
 provider "aws" {}
+
+resource "aws_lambda_function" "example" {
+  filename         = "lambda_function.zip"
+  function_name    = "example_lambda"
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "index.handler"
+  runtime         = "nodejs14.x"
+  environment {
+    variables = {
+      ENV = var.environment
+    }
+  }
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
