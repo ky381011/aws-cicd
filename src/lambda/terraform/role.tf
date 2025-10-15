8addc0097b5d64d3238c55c1fd1aade7a7b3920b
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-10-15 21:28:29 +0900

    Add role and minimum policy

---
+++ src/lambda/terraform/role.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-basic-execution"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Effect = "Allow"
+    }]
+  })
+}
