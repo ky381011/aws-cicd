82ed0cbbb11362e43e017a3c6746485f2a95730f
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-11-26 21:08:06 +0900

    Add subnet

--- src/ecs/terraform/networks.tf
+++ src/ecs/terraform/networks.tf
@@ -1,7 +1,26 @@
-resource "aws_vpc" "test_vpc"{
+resource "aws_vpc" "vpc"{
   cidr_block           = "172.16.0.0/20"
   enable_dns_hostnames = true
   tags = {
     Name = "tf_test"
+    deploy = "github_actions"
+  }
+}
+
+variable "subnet_cidrs" {
+  default = {
+    subnet0  = "172.16.0.0/24"
+  }
+}
+
+resource "aws_subnet" "subnets" {
+  for_each = var.subnet_cidrs
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = each.value
+
+  tags = {
+    Name = "tf_test"
+    deploy = "github_actions"
   }
 }
