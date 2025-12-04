3f5a95706e22751c9f7e63c7df64729a7e5f816b
ky381011 <135294108+ky381011@users.noreply.github.com>
2025-12-04 22:06:48 +0900

    Add security group

--- src/ecs/terraform/networks.tf
+++ src/ecs/terraform/networks.tf
@@ -21,3 +21,14 @@ resource "aws_network_interface" "ec2_nic" {
 
   tags = var.tags
 }
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
