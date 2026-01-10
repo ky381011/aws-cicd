170106198d9b70552ea896724eff0f51741eba97
ky381011 <ky381011git@gmail.com>
2026-01-10 23:51:25 +0900

    Update CloudWatch dashboard to include EC2 network metrics

--- src/ecs/terraform/monitor.tf
+++ src/ecs/terraform/monitor.tf
@@ -4,5 +4,9 @@
 
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.cloudwatch.dashboard_name
-  dashboard_body = jsonencode(var.cloudwatch.dashboard_body)
+  dashboard_body = replace(
+    jsonencode(var.cloudwatch.dashboard_body),
+    "$${instance_id}",
+    aws_instance.ec2.id
+  )
 }
